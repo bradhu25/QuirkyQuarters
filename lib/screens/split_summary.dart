a6eef159fd096604574dce36d5d935411fdbbe95
Iris Fu <iristfu@gmail.com>
2024-03-04 20:51:17 -0800

    Changed 'square up' to 'next' on split summary according to feedback

--- lib/screens/split_summary.dart
+++ lib/screens/split_summary.dart
@@ -113,7 +113,7 @@ class _SplitSummaryRouteState extends State<SplitSummaryRoute> {
         children: [
           ...payerTiles,
           ElevatedButton(
-            child: Text('Square Up'),
+            child: Text('Next'),
             onPressed: () {
               Navigator.push(
                 context,
