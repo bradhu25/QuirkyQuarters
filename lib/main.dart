b2dd287886b7cdc32818c4e0479f2cb27e6119c9
Bradley Hu <bradleyh@stanford.edu>
2024-03-11 01:22:10 -0700

    customized main theme, font, othr app styles

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 Future<void> main () async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +13,47 @@ Future<void> main () async {
 
 runApp(MaterialApp( // routes not const
     title: 'Quirky Quarters',
-    // home: HomePage(), // moved to routes
+    theme: ThemeData(
+      colorScheme: ColorScheme.light(
+        primary: Colors.teal[300]!, // A lighter shade of teal as the primary color
+        onPrimary: Colors.white, // For text/icons on top of the primary color
+        secondary: Colors.cyan[200]!, // A lighter shade of cyan for the secondary color
+        onSecondary: Colors.black, // For text/icons on top of the secondary color
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.teal[300], // Use the primary color from the color scheme
+        iconTheme: IconThemeData(color: Colors.white), // Icon color in the app bar
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: Colors.cyan[200],
+      ),
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.teal[300], // Default button color
+        textTheme: ButtonTextTheme.primary,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.teal[300]), // Background color for elevated buttons
+          foregroundColor: MaterialStateProperty.all(Colors.white), // Text color for elevated buttons
+        ),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.teal[300]), // Text color for text buttons
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.teal[300]!, width: 2.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.teal[300]!, width: 1.0),
+        ),
+      ),
+      // Fonts
+      textTheme: GoogleFonts.montserratTextTheme(),
+    ),
     routes: {
       '/': (context) => HomePage(),
     },
