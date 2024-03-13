2c0d28b34d9ff8ddd4c726451feb8042afab7764
Bradley Hu <bradleyh@stanford.edu>
2024-03-13 02:03:43 -0700

    OCR and photo functionality

---
+++ lib/text_scanner.dart
@@ -0,0 +1,93 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+class TextScannerPage extends StatefulWidget {
+  final String imagePath;
+
+  const TextScannerPage({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  State<TextScannerPage> createState() => _TextScannerPageState();
+}
+
+class _TextScannerPageState extends State<TextScannerPage> {
+  String _recognizedText = "Scanning...";
+  final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+
+  @override
+  void initState() {
+    super.initState();
+    _performTextRecognition();
+  }
+
+  @override
+  void dispose() {
+    textRecognizer.close();
+    super.dispose();
+  }
+
+  Future<void> _performTextRecognition() async {
+    try {
+      final inputImage = InputImage.fromFilePath(widget.imagePath);
+      final recognizedText = await textRecognizer.processImage(inputImage);
+        
+      setState(() {
+        print("Recognized text before: $_recognizedText");
+        _recognizedText = recognizedText.text;
+        print("Recognized text after: $_recognizedText");
+      });
+
+      
+      // Show the dialog after text recognition
+      WidgetsBinding.instance.addPostFrameCallback((_) => _showRecognizedTextDialog(context, _recognizedText));
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred when scanning text'),
+        ),
+      );
+    }
+  }
+
+  void _showRecognizedTextDialog(BuildContext context, String text) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Recognized Text'),
+          content: SingleChildScrollView(
+            child: Text(text),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text Scanner'),
+      ),
+      body: Center(
+        child: _recognizedText == "Scanning..."
+            ? Image.network(widget.imagePath)
+            : Text(_recognizedText)
+            // ? const CircularProgressIndicator()
+            // : SingleChildScrollView(
+            //     child: Text(_recognizedText),
+            //   ),
+      ),
+    );
+  }
+}
